4c0a5f8b833b57c1d658fcbfbf23db56477aa67c
Michael Mead <mmead.developer@gmail.com>
2022-06-13 09:14:02 -0700

    chore: delete commented code
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- zsh/vim.zsh
+++ zsh/vim.zsh
@@ -2,20 +2,3 @@
 
 bindkey -v
 bindkey -M viins 'jk' vi-cmd-mode
-
-function zle-keymap-select () {
-    case $KEYMAP in
-        vicmd) echo -ne '\e[1 q';;
-        viins|main) echo -ne '\e[5 q';;
-    esac
-}
-
-zle -N zle-keymap-select
-zle-line-init() {
-    zle -K viins
-    echo -ne "\e[5 q"
-}
-
-zle -N zle-line-init
-echo -ne '\e[5 q'
-preexec() { echo -ne '\e[5 q' ;}
