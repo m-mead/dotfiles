33ce3ae7560b53ce97d37e0bb0ba8714fa6387cb
Michael Mead <mmead.developer@gmail.com>
2023-04-12 18:03:13 -0700

    feat(nvim): add debug adapter protocol support
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -117,6 +117,7 @@ require("lazy").setup({
   'hrsh7th/nvim-cmp',
   'hrsh7th/vim-vsnip',
   'lewis6991/gitsigns.nvim',
+  'mfussenegger/nvim-dap',
   'neovim/nvim-lspconfig',
   'nvim-lua/plenary.nvim',
   'nvim-lualine/lualine.nvim',
@@ -177,7 +178,12 @@ vim.keymap.set('n', '<leader>sj', telescope_builtin.jumplist, { desc = '[S]earch
 
 -- Setup mason package manager for LSP tools (servers, linters, etc).
 require('mason').setup()
-require('mason-lspconfig').setup()
+
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'clangd',
+  }
+})
 
 -- Automcplete for nvim APIs -- must be setup before lspconfig.
 require("neodev").setup({})
@@ -361,6 +367,63 @@ require('gitsigns').setup({
 
 -- Zen mode
 vim.keymap.set('n', '<leader>Z', function() require('zen-mode').toggle() end, { desc = '[S]earch [H]elp' })
+
+-- Debug adapter
+local dap = require('dap')
+
+-- C++ configuration
+dap.adapters.cppdbg = {
+  id = 'cppdbg',
+  type = 'executable',
+  command = 'OpenDebugAD7',
+}
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
+
+-- dap keybindings
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<leader>db', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<leader>dB', function() require('dap').set_breakpoint() end)
+-- vim.keymap.set( 'n', '<leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<leader>dl', function() require('dap').run_last() end)
+vim.keymap.set({ 'n', 'v' }, '<leader>dh', function() require('dap.ui.widgets').hover() end)
+vim.keymap.set({ 'n', 'v' }, '<leader>dp', function() require('dap.ui.widgets').preview() end)
+
+vim.keymap.set('n', '<leader>df', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
+end)
+
+vim.keymap.set('n', '<leader>ds', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
+end)
 
 -- Async commands
 local function dispatch(args)
