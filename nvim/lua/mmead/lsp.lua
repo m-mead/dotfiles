b9985ad5136e96cf2a4cae78069f3b785f0a3af1
Michael Mead <mmead.developer@gmail.com>
2023-04-21 08:32:23 -0700

    fix(nvim): use arrow keys for completion menu
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- nvim/lua/mmead/lsp.lua
+++ nvim/lua/mmead/lsp.lua
@@ -93,7 +93,9 @@ end
 cmp.setup({
   mapping = {
     ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+    ["<Down>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
     ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+    ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-e>'] = cmp.mapping.abort(),
