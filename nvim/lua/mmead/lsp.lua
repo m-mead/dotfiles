2f5d5359bbae386a8b9b6c5b12b7208c926d2a3a
Michael Mead <mmead.developer@gmail.com>
2023-12-18 19:53:38 -0800

    chore: change inlay hint keybinding
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- nvim/lua/mmead/lsp.lua
+++ nvim/lua/mmead/lsp.lua
@@ -29,7 +29,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<f3>', '<cmd>ClangdSwitchSourceHeader<CR>', opts)
 
   if vim.lsp.inlay_hint then
-    vim.keymap.set("n", "<leader>uh", function()
+    vim.keymap.set("n", "<leader>hh", function()
       vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled(0))
     end, { desc = "Toggle inlay hints" })
   end
