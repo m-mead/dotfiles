c9e6507514c220b70c8025c6f942a41338e6574b
Michael Mead <mmead.developer@gmail.com>
2024-02-28 08:33:35 -0800

    wezterm: add color theme detection
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,9 +3,18 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'GruvboxDark'
+local wezterm = require "wezterm"
 
-config.font = wezterm.font('JetBrains Mono')
+function scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    return "GruvboxDark"
+  else
+    return "Catppuccin Latte"
+  end
+end
+
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.font = wezterm.font('JetBrainsMono Nerd Font')
 config.font_size = 13.0
 
 return config
