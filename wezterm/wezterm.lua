17b5445d1729a0847e8c9520b47abef00d10b305
Michael Mead <mmead.developer@gmail.com>
2024-02-28 21:30:48 -0800

    wezterm: set cursor_style to SteadyBlock
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,8 +3,6 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-local wezterm = require "wezterm"
-
 function scheme_for_appearance(appearance)
   if appearance:find "Dark" then
     return "GruvboxDark"
@@ -16,5 +14,7 @@ end
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 config.font = wezterm.font('JetBrainsMono Nerd Font')
 config.font_size = 13.0
+
+config.default_cursor_style = 'SteadyBlock'
 
 return config
