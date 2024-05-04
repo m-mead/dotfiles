7d5820b34ebdc7a114bb2fd5baf5c86818a1db5b
Michael Mead <mmead.developer@gmail.com>
2024-05-04 08:49:55 -0700

    nvim: add config descriptions for dap

--- nvim/lua/mmead/dap.lua
+++ nvim/lua/mmead/dap.lua
@@ -33,13 +33,13 @@ dap.configurations.cpp = {
 }
 
 -- dap keybindings
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end, { desc = 'continue' })
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end, { desc = 'step over' })
-vim.keymap.set('n', '<F11>', function() require('dap').step_into() end, { desc = 'step into' })
-vim.keymap.set('n', '<F12>', function() require('dap').step_out() end, { desc = 'step out' })
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end, { desc = 'debug continue' })
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end, { desc = 'debug step over' })
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end, { desc = 'debug step into' })
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end, { desc = 'debug step out' })
 vim.keymap.set('n', '<leader>db', function() require('dap').toggle_breakpoint() end, { desc = 'debug breakpoint toggle' })
 vim.keymap.set('n', '<leader>dB', function() require('dap').set_breakpoint() end, { desc = 'debug breakpoint set' })
-vim.keymap.set('n', '<leader>dr', function() require('dap').repl.open() end, { desc = 'debug repl' })
+vim.keymap.set('n', '<leader>dr', function() require('dap').repl.open() end, { desc = 'debug open repl' })
 vim.keymap.set('n', '<leader>dl', function() require('dap').run_last() end, { desc = 'debug ran last' })
 vim.keymap.set({ 'n', 'v' }, '<leader>dh', function() require('dap.ui.widgets').hover() end, { desc = 'debug hover' })
 vim.keymap.set({ 'n', 'v' }, '<leader>dp', function() require('dap.ui.widgets').preview() end, { desc = 'debug preview' })
@@ -47,9 +47,9 @@ vim.keymap.set({ 'n', 'v' }, '<leader>dp', function() require('dap.ui.widgets').
 vim.keymap.set('n', '<leader>df', function()
   local widgets = require('dap.ui.widgets')
   widgets.centered_float(widgets.frames)
-end, { desc = 'debug frames' })
+end, { desc = 'debug open frames' })
 
 vim.keymap.set('n', '<leader>ds', function()
   local widgets = require('dap.ui.widgets')
   widgets.centered_float(widgets.scopes)
-end, { desc = 'debug scopes' })
+end, { desc = 'debug open scopes' })
