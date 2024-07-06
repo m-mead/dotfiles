83476cd1d75227ffcd9f74cc97eb8891fccacb09
Michael Mead <mmead.developer@gmail.com>
2024-07-06 15:07:31 -0700

    nvim: enable treesitter context

--- nvim/lua/mmead/treesitter.lua
+++ nvim/lua/mmead/treesitter.lua
@@ -87,7 +87,7 @@ require('nvim-treesitter.configs').setup({
   },
 })
 
-require 'treesitter-context'.setup { enable = false }
+require 'treesitter-context'.setup { enable = true }
 
 vim.keymap.set('n', '<leader>tt', '<cmd>TSContextToggle<cr>',
   { noremap = true, silent = true, desc = 'Toggle treesitter context' })
