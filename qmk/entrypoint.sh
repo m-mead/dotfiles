56cff49925fef7578d80c9dbf60721242d132468
Michael Mead <mmead.developer@gmail.com>
2024-11-24 12:11:08 -0700

    qmk: remove duplicate packages
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- qmk/entrypoint.sh
+++ qmk/entrypoint.sh
@@ -4,7 +4,7 @@ if [[ -z "${KEYBOARD}" || -z "${KEYMAP}" ]]; then
 		echo -e "usage: entrypoint.sh"
 		echo -e "the following must be set in the environment:"
 		echo -e "	KEYBOARD (current: ${KEYBOARD})"	
-		echo -e "	KEYMAP (current: ${KEYMAP})"	
+		echo -e "	KEYMAP   (current: ${KEYMAP})"	
 		exit 1
 fi
 
@@ -13,7 +13,7 @@ echo "building qmk firmware for keyboard configuration ${KEYBOARD}:${KEYMAP}"
 qmk setup
 qmk compile -kb ${KEYBOARD} -km ${KEYMAP} --clean
 
-cp ${KEYBOARD}*.hex /qmk_bin
-cp ${KEYBOARD}*.uf2 /qmk_bin
+cp ${KEYBOARD}*.hex /qmk_bin > /dev/null 2>&1
+cp ${KEYBOARD}*.uf2 /qmk_bin > /dev/null 2>&1
 
 echo "firmware files: $(ls /qmk_bin)"
