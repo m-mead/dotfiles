dfd10143d86b84b080e60a00e35f7458fcd102d8
Michael Mead <mmead.developer@gmail.com>
2025-02-05 21:01:03 -0700

    nvim: move custom code to plugin/

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,26 @@
-require('custom.global')
-require('custom.lazy')
-require('custom.cmake')
-require('custom.python')
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Bootstrap lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=v11.16.2",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup plugins
+require("lazy").setup(
+  { import = "custom.plugins" },
+  {
+    change_detection = {
+      notify = false,
+    },
+  })
--- nvim/lua/custom/python.lua
+++
@@ -1,2 +0,0 @@
-vim.api.nvim_create_user_command('Black', function(_) vim.cmd('!black %') end,
-  { nargs = 0 })
