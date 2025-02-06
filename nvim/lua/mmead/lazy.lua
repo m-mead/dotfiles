07bebe375dba5bc3490dfb68ba45b2ec30f60f1f
Michael Mead <mmead.developer@gmail.com>
2025-02-05 20:50:57 -0700

    nvim: move telescope to plugin spec

--- nvim/lua/mmead/lazy.lua
+++ nvim/lua/mmead/lazy.lua
@@ -16,7 +16,4 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup plugins
 require("lazy").setup({
   { import = 'mmead.plugins' },
-  { 'nvim-lua/plenary.nvim', commit = '2d9b06177a975543726ce5c73fca176cedbffe9d' },      -- Latest commit as of 11/23/2024
-  { 'nvim-telescope/telescope.nvim', version = '0.1.8' },                                -- Latest stable as of 11/23/2024
-  { 'nvim-treesitter/nvim-treesitter', version = '0.9.3' },                              -- Latest stable as of 11/23/2024
 })
