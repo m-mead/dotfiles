4a47182cc0a8939d998e4bb93a7196a20b8706a8
Michael Mead <mmead.developer@gmail.com>
2025-02-05 19:22:09 -0700

    nvim: move lspconfig to own plugin spec

--- nvim/lua/mmead/lazy.lua
+++ nvim/lua/mmead/lazy.lua
@@ -16,7 +16,6 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup plugins
 require("lazy").setup({
   { import = 'mmead.plugins' },
-  { 'neovim/nvim-lspconfig', version = '1.6.0' },                                        -- Latest stable as of 02/03/2025
   { 'nvim-lua/plenary.nvim', commit = '2d9b06177a975543726ce5c73fca176cedbffe9d' },      -- Latest commit as of 11/23/2024
   { 'nvim-lualine/lualine.nvim', commit = '2a5bae925481f999263d6f5ed8361baef8df4f83' },  -- Latest commit as of 11/23/2024
   { 'nvim-telescope/telescope.nvim', version = '0.1.8' },                                -- Latest stable as of 11/23/2024
