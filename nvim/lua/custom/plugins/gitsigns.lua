39209cdc3ce32d2db9b371e0f5312e9ad9bef7b6
Michael Mead <mmead.developer@gmail.com>
2025-03-06 19:53:40 -0700

    nvim: bump plugins
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- nvim/lua/custom/plugins/gitsigns.lua
+++ nvim/lua/custom/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 return {
   'lewis6991/gitsigns.nvim',
-  version = '1.0.0',
+  version = '1.0.1',
   config = function()
     require('gitsigns').setup({
       signcolumn = true,
--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -1,6 +1,6 @@
 return {
   'stevearc/oil.nvim',
-  version = '2.14.0',
+  version = '2.15.0',
   config = function()
     require('oil').setup()
   end,
