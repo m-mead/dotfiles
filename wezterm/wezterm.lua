5378a92d9dd68c83750ae298fda5ed99d4023efa
Michael Mead <mmead.developer@gmail.com>
2025-04-04 19:44:35 -0600

    feat: switch to using vim kbd in zsh
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,23 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 
 local config = wezterm.config_builder()
 
-function scheme_for_appearance(appearance)
+local function scheme_for_appearance(appearance)
   if appearance:find "Dark" then
-    return 'tokyonight_night'
+    return "tokyonight_night"
   else
     return "Catppuccin Latte"
   end
 end
 
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
-config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 13.5
 
-config.default_cursor_style = 'SteadyBlock'
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.default_cursor_style = "SteadyBlock"
+config.cursor_blink_rate = 0
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 config.audible_bell = "Disabled"
 
--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,1 +1,27 @@
 bindkey '^R' history-incremental-search-backward
+
+# Vim keybindings
+bindkey -v
+
+# Block cursor on normal mode; line on insert
+function zle-keymap-select {
+  case $KEYMAP in
+    vicmd)      echo -ne '\e[1 q' ;;
+    viins|main) echo -ne '\e[5 q' ;;
+  esac
+}
+
+function zle-line-init {
+  zle-keymap-select
+}
+
+zle -N zle-keymap-select
+zle -N zle-line-init
+
+
+function zle-line-init {
+  zle-keymap-select
+}
+
+zle -N zle-keymap-select
+zle -N zle-line-init
