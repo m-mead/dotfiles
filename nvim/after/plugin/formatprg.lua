d579d5db72a9465d758bb5c5da5840b9ef8e0413
Michael Mead <mmead.developer@gmail.com>
2026-03-07 09:30:47 -0700

    nvim: set prettier as formatprg when working with typescript like files

---
+++ nvim/after/plugin/formatprg.lua
@@ -0,0 +1,39 @@
+local function set_typescript_formatprg()
+  if vim.fn.executable("prettier") ~= 1 then
+    return
+  end
+
+  local buffer = vim.api.nvim_get_current_buf()
+  local filename = vim.api.nvim_get_name(buffer)
+
+  vim.system(
+    { "prettier", "--find-config-path", filename },
+    {},
+    function(result)
+      local config = vim.trim(result.stdout or "")
+      if result.code ~= 0 then
+        return
+      end
+
+      if vim.fn.filereadable(config) ~= 1 then
+        return
+      end
+
+      vim.bo[buffer].formatprg = string.format("prettier --config %s --stdin-filepath %s", config, filename)
+      vim.bo[buffer].formatexpr = ""
+    end
+  )
+end
+
+local group = vim.api.nvim_create_augroup("FormatprgCustom", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = group,
+  pattern = {
+    "typescript",
+    "typescriptreact",
+    "javascript",
+    "javascriptreact",
+  },
+  callback = set_typescript_formatprg()
+})
