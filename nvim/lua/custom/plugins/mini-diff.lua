48a9db16e91ab030c1041db27e3f4ab54796872f
Michael Mead <mmead.developer@gmail.com>
2026-03-08 11:17:58 -0600

    nvim: use sign for mini-diff
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- nvim/lua/custom/plugins/mini-diff.lua
+++ nvim/lua/custom/plugins/mini-diff.lua
@@ -2,6 +2,11 @@ return {
   "nvim-mini/mini.diff",
   commit = "fbb93ea1728e7c9d0944df8bd022a68402bd2e7e", -- v0.17.0
   config = function()
-    require("mini.diff").setup({})
+    require("mini.diff").setup({
+      view = {
+        style = "sign",
+        signs = { add = "|", change = "|", delete = "|" },
+      }
+    })
   end,
 }
