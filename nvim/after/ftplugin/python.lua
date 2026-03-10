22d961ec5383b71e14c21b8ea14adda53fa160cc
Michael Mead <mmead.developer@gmail.com>
2026-03-09 19:50:50 -0600

    nvim: fix mismatched table keys
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -4,12 +4,13 @@ vim.opt_local.softtabstop = 4
 vim.opt_local.tabstop = 4
 
 local formatters = {
-  { executable = "ruff", command = "ruff format --stdin-filename % -" },
-  { exe = "black",       command = "black -q -" },
+  { executable = "ruff",  command = "ruff format --stdin-filename % -" },
+  { executable = "black", command = "black -q -" },
 }
 
 for _, formatter in ipairs(formatters) do
   if vim.fn.executable(formatter.executable) == 1 then
     vim.bo.formatprg = formatter.command
+    return
   end
 end
