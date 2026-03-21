31931203933e563e413dc048e93f559de47e6adf
Michael Mead <mmead.developer@gmail.com>
2026-03-21 10:48:03 -0600

    nvim: fix stray char
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- nvim/lua/custom/plugins/eddy.lua
+++ nvim/lua/custom/plugins/eddy.lua
@@ -1,6 +1,6 @@
 return {
   "m-mead/eddy.nvim",
-  commit = "48af26d73caae82f696eba08acb3393b380f5551 (",
+  commit = "48af26d73caae82f696eba08acb3393b380f5551",
   config = function()
     vim.cmd.colorscheme("eddy")
   end
