8e1eb28b14ac8544a4e5a9633c7242ae680d6d6f
Michael Mead <mmead.developer@gmail.com>
2026-03-22 21:01:32 -0600

    feat: update qmk scripts to build for either corne or lily58
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- qmk/README.md
+++ qmk/README.md
@@ -11,28 +11,16 @@ Install the dependencies
 make dep
 ```
 
-Run `make` to build the firmware.
+Run `make build-{keyboard}` to build the firmware.
 This step will use `docker-compose` to build the Docker image, mount the `qmk_firmware` repo, and compile the firmware.
 
 ```shell
-make
+make build-corne
+make build-lily58
 ```
 
 The firmware will be placed in `./bin`.
 
 ```shell
 ls -lah bin/
-```
-
-## Changing the keyboard
-
-The keyboard and keymap are specified in `docker-compose.yml` and can be modified.
-
-```yml
-    environment:
-      # Required
-      - KEYBOARD="lily58"
-      - KEYMAP="default"
-      # Optional
-      - CONVERT_TO="promicro_rp2040"
 ```
--- qmk/entrypoint.sh
+++ qmk/entrypoint.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-if [[ -z "${KEYBOARD}" || -z "${KEYMAP}" ]]; then
-		echo -e "usage: entrypoint.sh"
-		echo -e "the following must be set in the environment:"
-		echo -e "	KEYBOARD (current: ${KEYBOARD})"	
-		echo -e "	KEYMAP   (current: ${KEYMAP})"	
-		exit 1
+if [[ -z "${KEYBOARD}" || -z "${KEYMAP}" || -z "${CONVERT_TO}" ]]; then
+    echo -e "usage: entrypoint.sh"
+    echo -e "the following must be set in the environment:"
+    echo -e "	KEYBOARD  (current: ${KEYBOARD})"
+    echo -e "	KEYMAP    (current: ${KEYMAP})"
+    echo -e "   CONVET_TO (current: ${CONVERT_TO})"
+    exit 1
 fi
 
-echo "building qmk firmware for keyboard configuration ${KEYBOARD}:${KEYMAP}"
+echo "building qmk firmware for keyboard configuration ${KEYBOARD}:${KEYMAP} (convert to: ${CONVERT_TO})"
 
 qmk setup
-qmk compile -kb ${KEYBOARD} -km ${KEYMAP} --clean
+qmk compile -kb ${KEYBOARD} -km ${KEYMAP} --clean -e CONVERT_TO=${CONVERT_TO}
 
 cp ${KEYBOARD}*.hex /qmk_bin > /dev/null 2>&1
 cp ${KEYBOARD}*.uf2 /qmk_bin > /dev/null 2>&1
