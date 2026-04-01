c177120b55404acaf595ba219b1f0166c388a878
Michael Mead <mmead.developer@gmail.com>
2026-03-31 19:16:25 -0600

    nvim: enable semanticTokens and staticcheck
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- nvim/lsp/gopls.lua
+++ nvim/lsp/gopls.lua
@@ -2,4 +2,10 @@ return {
   cmd = { "gopls" },
   filetypes = { "go", "gomod" },
   root_markers = { "go.work", "go.mod" },
+  settings = {
+    gopls = {
+      semanticTokens = true,
+      staticcheck = true,
+    },
+  },
 }
