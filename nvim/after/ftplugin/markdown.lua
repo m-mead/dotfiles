47a91bed478e60846098fbac9632d2b10b6afd67
Michael Mead <mmead.developer@gmail.com>
2026-04-03 20:43:07 -0600

    nvim: add markdown fenced langs
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -4,3 +4,14 @@ vim.opt_local.expandtab = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.tabstop = 4
+
+vim.g.markdown_fenced_languages = {
+  "bash",
+  "c",
+  "cpp",
+  "go",
+  "lua",
+  "python",
+  "rust",
+  "sh",
+}
