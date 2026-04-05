605fd9f7addc587e88aec9a244375806d697a797
Michael Mead <mmead.developer@gmail.com>
2026-04-05 16:33:23 -0600

    nvim: update eddy.nvim to latest
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- nvim/lua/custom/plugins/thirdparty/eddy.lua
+++ nvim/lua/custom/plugins/thirdparty/eddy.lua
@@ -1,6 +1,6 @@
 return {
   src = "https://github.com/m-mead/eddy.nvim",
-  version = "5e1427ab3b9e2cf093a13e2f30856c0370696417",
+  version = "b6936e9481df1edd076e05c443c9673bd7b59607",
   config = function()
     vim.cmd.colorscheme("eddy")
   end
